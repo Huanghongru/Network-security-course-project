ea2dbb841dfc59af7e10adc604515be2813d5e59
huanghongru <onedesire@sjtu.edu.cn>
2020-05-09 23:45:20 +0800

    complete client server communication

---
+++ client_server.py
@@ -0,0 +1,118 @@
+import os, random
+
+from rsa import RSA
+from Crypto.Cipher import AES
+from typing import Dict, List, Tuple
+
+def WUP(content: str, mac: str, imei: str) -> List[str]:
+    """
+    The format of the WUP request
+    | ID - 4 bytes | content - 1024 bytes | mac - 4 bytes | imei - 5 bytes |
+
+    If the content is longer than 1024 bytes, we truncate it to pieces.
+    """
+    ret = []
+    wup_id = str(random.randint(1000, 9999))
+    for i in range(0, len(content), 1024):
+        sub_content = content[i: min(len(content), i + 1024)]
+        if len(sub_content) < 1024:
+            sub_content += " " * (1024 - len(sub_content))
+        ret.append("\t".join([wup_id, sub_content, mac, imei]))
+    return ret
+
+
+class Client(object):
+    def __init__(self):
+        self.id = random.randint(0, 1 << 64)
+        self.rsa = RSA()    # RSA key pair of this user
+        self.rsa.generate_key_pairs()
+
+        self.mac = str(random.randint(1000, 9999))   # MAC address of user
+        self.imei = str(random.randint(10000, 99999))  # IMEI series
+
+    def send_request(self, content) -> List[Tuple]:
+        """
+        return a made up WUP request
+        """
+        
+        # generate a 128-bit AES session key
+        aes_key = str(random.randint(1e15, 9e15))
+        aes = AES.new(aes_key)
+
+        # encrypt this session key using a 1024-bit RSA public key
+        encrypted_aes_key = self.rsa.encrypt(aes_key)
+
+        # use the AES session key to ecrypt the WUP request
+        wups = WUP(content, self.mac, self.imei)
+        encrypted_wups = [aes.encrypt(bytes(wup, 'utf-8')) for wup in wups]
+
+        # send the RSA-encrypted AES session key and the 
+        # encrypted WUP request to the server.
+        return [(self.id, encrypted_aes_key, wup) for wup in encrypted_wups]
+
+class Server(object):
+    def __init__(self):
+        self.client2rsa: Dict[int, RSA] = {}
+
+    def register(self, client: Client) -> None:
+        self.client2rsa[client.id] = client.rsa
+
+    def process_request(self, request: List[Tuple]) -> bool:
+        """
+        process a WUP request sended by the user. 
+
+        Args:
+        ----
+            request: a listi of WUP request
+        """
+        request_user_id, encrypted_aes_key, _ = request[0]
+
+        # decrypt the RSA-encrypted AES key it received from the client
+        if request_user_id not in self.client2rsa.keys():
+            print("The user of this request is not registered !")
+            return False
+        aes_key = self.client2rsa[request_user_id].decrypt(encrypted_aes_key)
+        aes = AES.new(aes_key)
+
+        # decrypt the WUP request using the AES session key
+        plain_text = ""
+        for _, _, req in request:
+            try:
+                # if we can't decode the content in utf-8 encoding.
+                # it should be an invalid WUP request
+                wup_id, content, mac, imei = aes.decrypt(req).decode('utf-8').split('\t')
+                plain_text += content.strip()
+            except:
+                print("Invalid WUP request")
+                return False
+        print(plain_text)
+        return True
+
+
+def test_communicate():
+    user1 = Client()
+    user2 = Client()
+    user3 = Client()
+    server = Server()
+    server.register(user1)
+    server.register(user2)
+
+    # user1 -> server
+    req1 = user1.send_request("hello world")
+
+    # user2 -> server
+    req2 = user2.send_request("network security")
+
+    # user3 -> server
+    req3 = user3.send_request("unregistered request")
+
+    # server -> user3
+    print(server.process_request(req3))
+
+    # server -> user2
+    print(server.process_request(req2))
+
+    # server -> user1
+    print(server.process_request(req1))
+
+test_communicate()
