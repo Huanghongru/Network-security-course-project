a9cea6d2b106b341a51c40aab39ad5e043adfd4b
huanghongru <onedesire@sjtu.edu.cn>
2020-05-13 15:10:26 +0800

    udpate main test code

--- main.py
+++ main.py
@@ -3,17 +3,18 @@ from client_server import test_communicate
 from attacker import test_hack
 
 def main():
-    # print("="*20, "Test for Part 1", "="*20)
-    # test_rsa("naive")
+    print("="*20, "Test for Part 1", "="*20)
+    test_rsa("naive")
 
-    # print("="*20, "Test for Part 2", "="*20)
-    # test_communicate()
+    print("="*20, "Test for Part 2", "="*20)
+    test_communicate()
     test_hack("naive")
 
     print("="*20, "Test for Part 3", "="*20)
-    # test_rsa("oaep")
-    # test_oaep_encode_and_decode()
+    test_rsa("oaep")
+    test_oaep_encode_and_decode()
 
+    # bonus attack on oaep-rsa
     test_hack("oaep")
 
 if __name__ == "__main__":
